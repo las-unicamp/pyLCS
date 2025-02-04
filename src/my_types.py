a7016040a5d6ee6c2c676174c48af4ce55558969
Renato Fuzaro Miotto <renato.fmiotto@gmail.com>
2025-02-04 18:14:56 -0300

    add(types): missing ArrayFloat32N

--- src/my_types.py
+++ src/my_types.py
@@ -1,5 +1,6 @@
 from nptyping import Float32, NDArray, Shape
 
+ArrayFloat32N = NDArray[Shape["*"], Float32]
 ArrayFloat32Nx2 = NDArray[Shape["*, 2"], Float32]
 ArrayFloat32Nx3 = NDArray[Shape["*, 3"], Float32]
 ArrayFloat32NxN = NDArray[Shape["*, *"], Float32]
